9fb0870b875639a8403bfb8fb957e38f069dd20f
Yan Wang <yan.wang.fire@gmail.com>
2018-09-13 15:47:13 +0800

    Add files via upload

---
+++ GENSIMDATA/SpherePointPicking.m
@@ -0,0 +1,18 @@
+function [theta,phi]=SpherePointPicking(n)
+%Uniform random Sphere Point Picking
+%r = 1;
+NN = n;
+u = random('uniform',0,1,1,NN);
+v = random('uniform',0,1,1,NN);
+theta = 2*pi*u;
+phi = acos(2*v-1)-pi/2;
+
+% x = zeros(3,1);
+% y = zeros(3,1);
+% z = zeros(3,1);
+% for i=1:1:100
+% x(i) = r*sin(phi(i))*cos(theta(i));
+% y(i) = r*sin(phi(i))*sin(theta(i));
+% z(i) = r*cos(phi(i));
+% end
+% plot3(x,y,z,'.r');
