4ac53b3c593cad79862b4e03dee2bc7ebc689170
Dr.QYQ <qyqstc@gmail.com>
2018-11-19 11:18:07 -0600

    remove the data from this folder

--- GENSIMDATA/mat2hdf5_script.m
+++ GENSIMDATA/mat2hdf5_script.m
@@ -1,14 +1,14 @@
 % convert .mat file to .hdf5 file
 
-inFileList = dir('TESTDATA/*.mat'); 
-mkdir('TESTDATA_HDF5')
+inFileList = dir('/Users/qianyiqian/Research/DATA/TESTDATA/*.mat'); % input file directory
+mkdir('/Users/qianyiqian/Research/DATA/TESTDATA_HDF5')% output directory
 for lpc = 1:length(inFileList) 
 
-inFile = ['TESTDATA',filesep,inFileList(lpc).name]; 
+inFile = ['/Users/qianyiqian/Research/DATA/TESTDATA',filesep,inFileList(lpc).name]; 
 
 [~,inFileName,~]  = fileparts(inFile); 
 
-outFile = ['TESTDATA_HDF5',filesep,inFileName,'.hdf5']; 
+outFile = ['/Users/qianyiqian/Research/DATA/TESTDATA_HDF5',filesep,inFileName,'.hdf5']; 
 
 mpavinfile2hdf5(inFile,outFile); 
 
