b40be45db300830266726d8ea660cf384252a7e3
Dr.QYQ <qyqstc@gmail.com>
2018-11-25 10:56:32 -0600

    udpate the searchParams convert scripts

--- GENSIMDATA/searchParam_mat2hdf5.m
+++ GENSIMDATA/searchParam_mat2hdf5.m
@@ -1,10 +1,11 @@
 % A script convert searchParams file from .mat to hdf5
 
-inFileList = dir('/Users/qianyiqian/Research/DATA/searchParams_GWBsimDataSKA/*.mat');
+inFileList = dir('/Users/qianyiqian/Research/PulsarTiming/SimDATA/searchParams_GWBsimDataSKA/*.mat');
+mkdir('/Users/qianyiqian/Research/PulsarTiming/SimDATA/searchParams_HDF5');
 for lpc = 1:length(inFileList)
-    inFile = ['/Users/qianyiqian/Research/DATA/searchParams_GWBsimDataSKA',filesep,inFileList(lpc).name];
+    inFile = ['/Users/qianyiqian/Research/PulsarTiming/SimDATA/searchParams_GWBsimDataSKA',filesep,inFileList(lpc).name];
     [~,inFileName,~] = fileparts(inFile);
-    outFileName = ['/Users/qianyiqian/Research/DATA/searchParams_GWBsimDataSKA',filesep,inFileName,'.hdf5'];
+    outFileName = ['/Users/qianyiqian/Research/PulsarTiming/SimDATA/searchParams_HDF5',filesep,inFileName,'.hdf5'];
     inFileInfo = load(inFile);
     fldName = 'xmaxmin';
     %Create HDF5 file
