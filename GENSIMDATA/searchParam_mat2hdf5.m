7ff1b2fbc6872d1b464798a4711898ed72eeca7b
Dr.QYQ <qyqstc@gmail.com>
2019-04-22 17:13:21 -0500

    update search parameter generate function

--- GENSIMDATA/searchParam_mat2hdf5.m
+++ GENSIMDATA/searchParam_mat2hdf5.m
@@ -1,19 +1,48 @@
 % A script convert searchParams file from .mat to hdf5
-
-inFileList = dir('/Users/qianyiqian/Research/PulsarTiming/SimDATA/searchParams_GWBsimDataSKA/*.mat');
-mkdir('/Users/qianyiqian/Research/PulsarTiming/SimDATA/searchParams_HDF5');
+clear;
+inFileList = dir('~/Research/PulsarTiming/SimDATA/Mauritius/uniform search params/*.mat');
+mkdir('~/Research/PulsarTiming/SimDATA/Mauritius/uniform search params HDF5');
 for lpc = 1:length(inFileList)
-    inFile = ['/Users/qianyiqian/Research/PulsarTiming/SimDATA/searchParams_GWBsimDataSKA',filesep,inFileList(lpc).name];
+    inFile = ['~/Research/PulsarTiming/SimDATA/Mauritius/uniform search params',filesep,inFileList(lpc).name];
     [~,inFileName,~] = fileparts(inFile);
-    outFileName = ['/Users/qianyiqian/Research/PulsarTiming/SimDATA/searchParams_HDF5',filesep,inFileName,'.hdf5'];
+    outFileName = ['~/Research/PulsarTiming/SimDATA/Mauritius/uniform search params HDF5',filesep,inFileName,'.hdf5'];
     inFileInfo = load(inFile);
+    struName = 'searchParams';
     fldName = 'xmaxmin';
+    bandsIn = 'Bands_Info';
     %Create HDF5 file
     fid = H5F.create(outFileName);
     H5F.close(fid);
-    nDim = length(size((inFileInfo.(fldName))'));
+    nDim = length(size((inFileInfo.(struName).alpha)'));
     baseSz = ones(1, nDim);
-    h5create(outFileName,['/',fldName],...
-        max([size((inFileInfo.(fldName))'); baseSz]));
-    h5write(outFileName,['/',fldName],(inFileInfo.(fldName))');
+    
+    h5create(outFileName,['/',bandsIn],2);
+    h5write(outFileName,['/',bandsIn],inFileInfo.(struName).band_num,1,1);
+    h5writeatt(outFileName,['/',bandsIn],'bandNumber',inFileInfo.(struName).band_num);
+    h5write(outFileName,['/',bandsIn],inFileInfo.NumBands,2,1);
+    h5writeatt(outFileName,['/',bandsIn],'NumBands',inFileInfo.NumBands);
+    
+%     h5create(outFileName,['/',fldName],...
+%         max([size((inFileInfo.(struName).alpha)'); baseSz]));
+    h5create(outFileName,['/',fldName],[7,2])
+    h5write(outFileName,['/',fldName],(inFileInfo.(struName).alpha)',[1,1],[1,2]);
+    h5writeatt(outFileName,['/',fldName],'alpha',(inFileInfo.(struName).alpha)');
+    
+    h5write(outFileName,['/',fldName],(inFileInfo.(struName).delta)',[2,1],[1,2]);
+    h5writeatt(outFileName,['/',fldName],'delta',(inFileInfo.(struName).delta)');
+    
+    h5write(outFileName,['/',fldName],(inFileInfo.(struName).angular_velocity)',[3,1],[1,2]);
+    h5writeatt(outFileName,['/',fldName],'angular velocity',(inFileInfo.(struName).angular_velocity)');
+    
+    h5write(outFileName,['/',fldName],(inFileInfo.(struName).phi0)',[4,1],[1,2]);
+    h5writeatt(outFileName,['/',fldName],'phi0',(inFileInfo.(struName).phi0)');
+    
+    h5write(outFileName,['/',fldName],(inFileInfo.(struName).amplitude)',[5,1],[1,2]);
+    h5writeatt(outFileName,['/',fldName],'amplitude',(inFileInfo.(struName).amplitude)');
+    
+    h5write(outFileName,['/',fldName],(inFileInfo.(struName).inclination)',[6,1],[1,2]);
+    h5writeatt(outFileName,['/',fldName],'inclination',(inFileInfo.(struName).inclination)');
+    
+    h5write(outFileName,['/',fldName],(inFileInfo.FreqRange)',[7,1],[1,2]);
+    h5writeatt(outFileName,['/',fldName],'Frequency range',(inFileInfo.FreqRange)');
 end
