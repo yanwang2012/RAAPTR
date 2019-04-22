5c28689e3bc7b10c38ad39f667261960d91d863d
Soumya Mohanty <soumya.mohanty@utb.edu>
2019-04-22 16:20:30 -0500

    Function perfevalomp2hdf5file modified to take mp_av_select input

--- MxAvPhaseC/perfeval_omp.h
+++ MxAvPhaseC/perfeval_omp.h
@@ -21,7 +21,7 @@ struct  llr_pso_params * loadfile2llrparam(hid_t);
 /* Dump output from multiple pso runs in perfeval to .hdf5 file */
 void perfevalomp2hdf5file(size_t, size_t, size_t, gsl_vector *,
 					  struct returnData *[], struct fitFuncParams *,
-                      double (*)(gsl_vector *, void *), hid_t );
+                      double (*)(gsl_vector *, void *), char *, hid_t );
 					  
 
 #endif
