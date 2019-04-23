7a490b24e02de47ba1297a701ff27fb44fccf42a
Dr.QYQ <qyqstc@gmail.com>
2019-04-22 21:06:21 -0500

    update launch job generate function

--- GENSIMDATA/genls5launcherjobfile.m
+++ GENSIMDATA/genls5launcherjobfile.m
@@ -1,11 +1,12 @@
-simDataDir = '~/Research/PulsarTiming/SimDATA/Maricius results/GWBsimDataSKA_HDF5'; % local
+clear;
+simDataDir = '~/Research/PulsarTiming/SimDATA/Mauritius/GWBsimDataSKA_HDF5'; % local
 %simDataDir = '/work/05884/qyqstc/lonestar/GWBsimDataSKA_HDF5'; % on ls5
-search_paramDataDir = '~/Research/PulsarTiming/SimDATA/MultiSource/searchParams_HDF5';
+search_paramDataDir = '~/Research/PulsarTiming/SimDATA/Mauritius/uniform search params HDF5';
 inFileList = dir([simDataDir,filesep,'*.hdf5']);
 inFileList_param = dir([search_paramDataDir,filesep,'*.hdf5']);
 nParamFiles = length(inFileList_param);
 nFiles = length(inFileList);
-fid = fopen('Maricius_JOBFILE.txt','w');
+fid = fopen('Maricius_UNI_JOBFILE.txt','w');
 
 %% single source
 for lpc = 1:nFiles
@@ -24,11 +25,13 @@ fclose(fid);
 %% multiple source
 for lpc = 1:nFiles
     for ppc = 1:nParamFiles
+        bandNum = h5readatt([search_paramDataDir,'/',inFileList_param(ppc).name],...
+            ['/','Bands_Info'],'bandNumber');
         fprintf(fid,'/work/05884/qyqstc/lonestar/PULSARTIMING/RAAPTR/MxAvPhaseC/perfeval_spmd.out ');
-        fprintf(fid,'/work/05884/qyqstc/lonestar/searchParams_HDF5/%s ',inFileList_param(ppc).name);
+        fprintf(fid,'/work/05884/qyqstc/lonestar/uniform_searchParams_HDF5/%s ',inFileList_param(ppc).name);
         fprintf(fid,'/work/05884/qyqstc/lonestar/GWBsimDataSKA_HDF5/%s ',inFileList(lpc).name);
-        fprintf(fid,'/work/05884/qyqstc/lonestar/Maricius_results/%d_%s ',ppc,inFileList(lpc).name);
-        fprintf(fid,'AvPhase');
+        fprintf(fid,'/work/05884/qyqstc/lonestar/Mauritius_uni_results/%d_%s ',bandNum,inFileList(lpc).name);
+        fprintf(fid,'avPhase');
         fprintf(fid,'\n');
     end
 end
