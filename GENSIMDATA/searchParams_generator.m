6ef1a29447183c3cddedc42901a6e586d0b7d5b8
Dr.QYQ <qyqstc@gmail.com>
2019-05-30 15:58:14 -0500

    daily improvement, added a new script mat2json.mat

--- GENSIMDATA/searchParams_generator.m
+++ GENSIMDATA/searchParams_generator.m
@@ -1,16 +1,20 @@
-% load the entire xmaxmin width from a txt file then split the frequency into 10 bands
+% load the entire xmaxmin width from a json file then split the frequency into 10 bands
 %%
-% load('searchParams_GWBsimDataSKA.mat')
-% writematrix(xmaxmin,'xmaxmin.txt','delimiter','tab');
-xmaxmin = readmatrix('xmaxmin.txt');
-bandwidth = xmaxmin(3,1)/10;
+clear;
+filename = 'Nyquist.json';
+outDir = 'searchParams_Nyquist';
+mkdir(outDir);
+searchParams = jsondecode(fileread(filename));
+NumBands = 10; % total number of bands
+bandwidth = searchParams.angular_velocity(1)/NumBands;% max frequency divided into 10 bands
+FreqRange = searchParams.angular_velocity;
 for i = 1:10
-    xmaxmin(3,1) = bandwidth*i;
+    searchParams.angular_velocity(1) = bandwidth*i;
     if i ==1
-        xmaxmin(3,2) = 1;
+        searchParams.angular_velocity(2) = 1;
     else
-        xmaxmin(3,2) = bandwidth*(i-1);
+        searchParams.angular_velocity(2) = bandwidth*(i-1);
     end
-    nband = i;
-    save(['searchParams_GWBsimDataSKA',num2str(i),'.mat'],'xmaxmin','nband');
+    searchParams.band_num = i;
+    save([outDir,filesep,'searchParams_Nyquist',num2str(i),'.mat'],'searchParams','NumBands','FreqRange');
 end
