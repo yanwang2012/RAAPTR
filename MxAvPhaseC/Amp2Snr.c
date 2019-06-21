6944fd46b18f98eca22420a3043c18992724a15c
Dr.QYQ <qyqstc@gmail.com>
2019-06-21 00:36:58 -0500

    update converting

--- MxAvPhaseC/Amp2Snr.c
+++ MxAvPhaseC/Amp2Snr.c
@@ -1,44 +1,41 @@
-#include "maxphase.h"
-#include "LLR_Mp_Av.h"
-#include "ptapso.h"
-#include "perfeval_omp.h"
+#include "hdf5.h"
 #include "gslhdf5_io.h"
-#include "hdf5_hl.h"
-#include "omp.h"
+#include "perfeval_omp.h"
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
-
 /*! \file
-\brief Convert amplitude to SNR and return the timing residuals of estimated source.
+/brief Subtract timing residuals of estimated source from input data and creat a new input data file.
+
 \author Yiqian Qian
-*/
+ */
 /*!
-This function reads the amplitude stored in estimated file and convert it back
-to SNR, mean while returns the timing residuals of the estimated source.
-*/
+This function reads the estimated file and calculate the corresponding SNR and timing residual of the estimated source. 
+Then subtract the timing residual from it and creat a new input data file.
+ */
 void Amp2Snr(char *inputFileName)
 {
 
-  // load data from .hdf5 file
-  herr_t status;
-  hid_t inFile = H5Fopen(inputFileName, H5F_ACC_RDONLY, H5P_DEFAULT);
-  if (inFile < 0)
-  {
-    fprintf(stdout, "Error openning file /n");
-    abort();
-  }
+    // load the .hdf5 file
+    herr_t status;
+    hid_t inFile = H5Fopen(inputFileName, H5F_ACC_RDONLY, H5P_DEFAULT);
+    if (inFile < 0)
+    {
+        fprintf(stdout, "Error opening file\n");
+        abort();
+    }
+    //Remaining data loads into special parameter structure
+    struct llr_pso_params *llp;
+    llp = loadfile2llrparam(inFile);
 
-  struct llr_pso_params *llp;
-  llp = loadfile2llrparam(inFile);
-
-  // Close file
-  status = H5Fclose(inFile);
-  if (status < 0)
-  {
-    fprintf(stdout, "Error closing file %s /n", inputFileName);
-  }
-};
+    //Close file
+    status = H5Fclose(inFile);
+    if (status < 0)
+    {
+        fprintf(stdout, "Error closing file %s \n", inputFileName);
+    }
+    
+}
