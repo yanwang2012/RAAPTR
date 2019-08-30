842a92c4a27e942ab37ffc6b3d17256a0f574a0d
Yiqian Qian <qyqstc@gmail.com>
2019-08-30 12:17:56 -0500

    update

--- MxAvPhaseC/build_test_subtract.sh
+++ MxAvPhaseC/build_test_subtract.sh
@@ -1,4 +1,4 @@
 gcc -O3 -c -I$TACC_HDF5_INC -I$TACC_GSL_INC gslhdf5_io.c
 gcc -O3 -c -I$TACC_HDF5_INC -I$TACC_GSL_INC subtract.c
 gcc -O3 -c -I$TACC_HDF5_INC -I$TACC_GSL_INC test_subtract.c
-gcc -O3 gslhdf5_io.o subtract.o test_subtract.o -L$TACC_HDF5_LIB -lhdf5 -lhdf5_hl -lz -L$TACC_GSL_LIB -lm -lgsl -lgslcblas -o subtract.out
+gcc -O3 gslhdf5_io.o subtract.o test_subtract.o -L$TACC_HDF5_LIB -lhdf5 -lhdf5_hl -lz -L$TACC_GSL_LIB -lm -lgsl -lgslcblas -o test_subtract.out
