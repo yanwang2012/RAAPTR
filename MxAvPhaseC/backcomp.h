528b23913d5913594ae5d289879add45b9e49226
Soumya Mohanty <soumya.mohanty@utb.edu>
2019-09-02 18:08:00 -0500

    Added header defined if block.

--- MxAvPhaseC/backcomp.h
+++ MxAvPhaseC/backcomp.h
@@ -1,6 +1,11 @@
+#if !defined(BACKCOMP_HDR)
+#define BACKCOMP_HDR
+
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
 #include <stddef.h>
 #include <stdio.h>
 
 gsl_matrix * raaptr_vec2mat(gsl_vector *, size_t);
+
+#endif
