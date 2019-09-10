bbbc0c67fc705f8404319fd8a0d4347da925e24b
Yiqian Qian <qyqstc@gmail.com>
2019-09-09 23:06:00 -0500

    update

--- MxAvPhaseC/build_test_subtract.sh
+++ MxAvPhaseC/build_test_subtract.sh
@@ -1,9 +1,9 @@
 # Breif script to compile test_subtract.out
-gcc -c -I$TACC_HDF5_INC -I$TACC_GSL_INC gslhdf5_io.c
-gcc -c -I$TACC_HDF5_INC -I$TACC_GSL_INC subtract.c
-gcc -c -I$TACC_HDF5_INC -I$TACC_GSL_INC test_subtract.c
-gcc -c -I$TACC_HDF5_INC -I$TACC_GSL_INC perfeval_omp.c
-gcc -c -I$TACC_HDF5_INC -I$TACC_GSL_INC maxphaseutils.c
-gcc -c -I$TACC_HDF5_INC -I$TACC_GSL_INC LLR_Mp_Av.c
-gcc -c -I$TACC_HDF5_INC -I$TACC_GSL_INC ptapso.c
-gcc -fopenmp perfeval_omp.o LLR_Mp_Av.o maxphaseutils.o ptapso.o gslhdf5_io.o subtract.o test_subtract.o -L$TACC_HDF5_LIB -lhdf5 -lhdf5_hl -lz -L$TACC_GSL_LIB -lm -lgsl -lgslcblas -o test_subtract.out
+gcc -g -c -I$TACC_HDF5_INC -I$TACC_GSL_INC gslhdf5_io.c
+gcc -g -c -I$TACC_HDF5_INC -I$TACC_GSL_INC subtract.c
+gcc -g -c -I$TACC_HDF5_INC -I$TACC_GSL_INC test_subtract.c
+gcc -g -c -I$TACC_HDF5_INC -I$TACC_GSL_INC perfeval_omp.c
+gcc -g -c -I$TACC_HDF5_INC -I$TACC_GSL_INC maxphaseutils.c
+gcc -g -c -I$TACC_HDF5_INC -I$TACC_GSL_INC LLR_Mp_Av.c
+gcc -g -c -I$TACC_HDF5_INC -I$TACC_GSL_INC ptapso.c
+gcc -g -fopenmp perfeval_omp.o LLR_Mp_Av.o maxphaseutils.o ptapso.o gslhdf5_io.o subtract.o test_subtract.o -L$TACC_HDF5_LIB -lhdf5 -lhdf5_hl -lz -L$TACC_GSL_LIB -lm -lgsl -lgslcblas -o test_subtract.out
