f189599ff1d1bf6c84aea736b7979f8770d787c6
Yiqian Qian <qyqstc@gmail.com>
2019-09-09 23:38:08 -0500

    debug

--- MxAvPhaseC/test_subtract.c
+++ MxAvPhaseC/test_subtract.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]){
 
     FILE * residuals;
     residuals = fopen("timingResiduals.txt","w");
-    gsl_matrix_fprintf(residuals,timResiduals,"%g");
+    gsl_matrix_fprintf(residuals,timResiduals,"%f");
     fclose(residuals);
 
     printParam(srcp);
