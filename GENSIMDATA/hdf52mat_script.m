2127083933d5929b03e793f762fd8b5497e6f60d
Yiqian Qian <qyqstc@gmail.com>
2020-07-01 17:51:23 -0500

    update

--- GENSIMDATA/hdf52mat_script.m
+++ GENSIMDATA/hdf52mat_script.m
@@ -1,6 +1,6 @@
 % A script convert .hdf5 file to .mat file
 clear;
-inputFileDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/SupNar_xMBLT_iMBLT20/GWBsimDataSKASrlz1Nrlz3_xMBLT2/results/1_iMBLT/results/1iMBLT_after/results/2_iMBLT/results/2iMBLT_after/results/3_iMBLT/results/3iMBLT_after/results/4_iMBLT/results/4iMBLT_after/results/5_iMBLT/results/5iMBLT_after/results/6_iMBLT/results/6iMBLT_after/results/7_iMBLT/results/7iMBLT_after/results/8_iMBLT/results';
+inputFileDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/SupNar_xMBLT_iMBLT20/GWBsimDataSKASrlz1Nrlz3_xMBLT2/results/1_iMBLT/results/1iMBLT_after/results/2_iMBLT/results/2iMBLT_after/results/3_iMBLT/results/3iMBLT_after/results/4_iMBLT/results/4iMBLT_after/results/5_iMBLT/results/5iMBLT_after/results/6_iMBLT/results/6iMBLT_after/results/7_iMBLT/results/7iMBLT_after/results/8_iMBLT/results/8iMBLT_after/results/9_iMBLT/results/9iMBLT_after/results/10_iMBLT/results';
 % outputFileDir = [inputFileDir,filesep,'MAT'];
 inFileList = dir([inputFileDir,filesep,'*.hdf5']); % input file directory
 % mkdir(outputFileDir)% output directory
