9fe617a5f7eec2fc0ce1a426d94ce1dcfac9a5c2
Yiqian Qian <qyq@Yiqians-MBP.lan1>
2020-07-28 16:05:08 -0500

    add text labeling to Est.vsEst.m

--- GENSIMDATA/EstvsEst.m
+++ GENSIMDATA/EstvsEst.m
@@ -1,5 +1,5 @@
 % Script to plot different parameters over different estimated results.
-% Yiqian 6.29 2020
+% Yiqian 6/29/2020
 
 clear;
 tic
@@ -7,8 +7,8 @@ tic
 %% Dir settings
 simParamsDir = '~/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/searchParams/2bands/superNarrow';
 simdataDir = '~/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands';
-estSrc1Dir = '~/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/Results_supNar/GWBsimDataSKASrlz1Nrlz3_xMBLT/results';
-estSrc2Dir = '~/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/supperNarrow_iMBLT20';
+estSrc1Dir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/Results_supNar';
+estSrc2Dir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/SupNar_xMBLT_iMBLT20/iMBLT20';
 Filename = 'GWBsimDataSKASrlz1Nrlz3';
 ext = '.mat';
 
@@ -40,8 +40,9 @@ end
 %% Plotting
 prefix = [estSrc2Dir,filesep,'EstvsEst'];
 mkdir(prefix)
+baseName = 'initial-iMBLT-after-xMBLT';
 % SNR
-figname1 = 'xMBLT-iMBLT-SNR';
+figname1 = [baseName,'-SNR'];
 snr1 = zeros(Nband,NestsrcBand);
 snr2 = zeros(Nband,NestsrcBand);
 for i = 1:Nband
@@ -51,22 +52,23 @@ for i = 1:Nband
     end
 end
 
+figure(1)
 subplot(1,2,1)
 plot(snr1(1,:),snr2(1,:),'ob')
 title('band 1')
-xlabel('xMBLT')
-ylabel('iMBLT')
+xlabel('initial')
+ylabel('iMBLT after xMBLT')
 subplot(1,2,2)
 plot(snr1(2,:),snr2(2,:),'or')
 title('band 2')
-xlabel('xMBLT')
-ylabel('iMBLT')
-sgtitle('xMBLT vs. iMBLT SNR')
+xlabel('initial')
+ylabel('iMBLT after xMBLT')
+sgtitle('initial vs. iMBLT after xMBLT SNR')
 saveas(gcf,[prefix,filesep,figname1],'png')
 savefig([prefix,filesep,figname1])
 
 % Frequency
-figname2 = 'xMBLT-iMBLT-Freq';
+figname2 = [baseName,'-Freq'];
 freq1 = zeros(Nband,NestsrcBand);
 freq2 = zeros(Nband,NestsrcBand);
 for i = 1:Nband
@@ -74,24 +76,37 @@ for i = 1:Nband
     freq2(i,:) = arrayfun(@(x) EstSrc2{i,x}.omega/(2*pi*365*24*3600), 1:NestsrcBand);
 end
 
+figure(2)
 subplot(1,2,1)
 plot(freq1(1,:),freq2(1,:),'ob')
 title('band 1')
-xlabel('xMBLT')
-ylabel('iMBLT')
+xlabel('initial')
+ylabel('iMBLT after xMBLT')
 xlim([0 4.5e-8])
 % ylim([0 4.5e-7])
 subplot(1,2,2)
 plot(freq1(2,:),freq2(2,:),'or')
 title('band 2')
-xlabel('xMBLT')
-ylabel('iMBLT')
-sgtitle('xMBLT vs. iMBLT Freq.')
+xlabel('initial')
+ylabel('iMBLT after xMBLT')
+sgtitle('initial vs. iMBLT after xMBLT Freq.')
 saveas(gcf,[prefix,filesep,figname2],'png')
 savefig([prefix,filesep,figname2])
 
-
-
+% SNR vs. Freq.
+figname3 = [baseName,'-SNRvsFreq'];
+for i = 1:2
+    figure(3+i-1)
+    plot(snr1(i,:),freq1(i,:),'ob',snr2(i,:),freq2(i,:),'sr');
+    text(snr1(i,:) + 2,freq1(i,:),num2str((1:numel(snr1(i,:)))'),'Color','#0072BD');
+    text(snr2(i,:) - 2,freq2(i,:),num2str((1:numel(snr2(i,:)))'),'Color','#D95319','HorizontalAlignment','right');
+    title([figname3,'Band ',num2str(i)]);
+    xlabel('SNR');
+    ylabel('Frequency');
+    legend('initial','iMBLTafterxMBLT');
+    saveas(gcf,[prefix,filesep,figname3,'Band ',num2str(i)],'png')
+    savefig([prefix,filesep,figname3,'Band ',num2str(i)])
+end
 
 toc
 
