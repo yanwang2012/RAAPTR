47c824155dc0cab5401987efc0acf664097c0677
Yiqian Qian <qyq@Yiqians-MBP.lan1>
2020-08-27 12:49:23 +0800

    add new scripts to generate new searchParams with fluctuation

--- GENSIMDATA/overlap_acceptance.m
+++ GENSIMDATA/overlap_acceptance.m
@@ -9,9 +9,12 @@ clear
 tic
 
 simParamsDir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/searchParams/2bands/superNarrow';
-outputDir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/searchParams/2bands/overlap-acceptance';
+outputDir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/searchParams/2bands/overlap2';
+mkdir(outputDir)
 searchRange = load([simParamsDir,filesep,'searchParams1.mat']);
-delta = (searchRange.searchParams.angular_velocity(1) - searchRange.searchParams.angular_velocity(2))/10; % overlaping frequency interval
+
+times =  2; % parameter to tune the gap
+delta = times * (searchRange.searchParams.angular_velocity(1) - searchRange.searchParams.angular_velocity(2))/10; % overlaping frequency interval
 range_low = searchRange.searchParams.angular_velocity(1) - delta;
 copyfile([simParamsDir,filesep,'searchParams1.mat'],[outputDir,filesep,'searchParams2.mat']);
 f = matfile([outputDir,filesep,'searchParams2.mat'],'Writable',true);
@@ -19,54 +22,54 @@ searchParams = f.searchParams;
 searchParams.angular_velocity(1,1) = searchRange.FreqRange(1); % upper limit of new band
 searchParams.angular_velocity(2,1) = range_low - delta; % lower limit of new band
 
-stage = 2; % number of band
+stage = 2; % band #
 
 searchParams.band_num = stage;
 f.searchParams = searchParams;
 
-%% Set up
-simDataDir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands';
-estDataDir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/Results_supNar';
-inputFileName = 'GWBsimDataSKASrlz1Nrlz3';
-ext = '.mat';
-
-
-% Load the simulated source parameters.
-load([simDataDir,filesep,inputFileName,ext],'simParams','yr');
-outputfiles = dir([estDataDir,filesep,num2str(stage - 1),'_',inputFileName,'*',ext]);
-NestSrc = length(outputfiles);
-outputfilenames = sort_nat({outputfiles.name});
-
-estTimRes = zeros(simParams.Np,simParams.N);
-ResCell = {}; % a cell of timing residuals to store all the estimated residuals.
-% SNRarray = [];
-
-for j = 1:NestSrc
-    
-    
-    path_estData = [estDataDir,filesep,char(outputfilenames(j))];
-    [srcParams]=ColSrcParams(path_estData);
-    if srcParams.omega < searchRange.searchParams.angular_velocity(1,1) && srcParams.omega > range_low
-        [~,estTimRes_tmp] = Amp2Snr(srcParams,simParams,yr);
-        ResCell = [ResCell estTimRes_tmp];
-        %     SNRarray = [SNRarray SNR];
-    else
-        continue
-    end
-    
-end
-
-% Accumulate the timing residulas for different sources.
-num_src = length(ResCell);
-for nsrc = 1:num_src
-    estTimRes = estTimRes + ResCell{nsrc};
-end
-
-newFile = [outputDir,filesep,num2str(stage),'_',inputFileName,ext];
-copyfile([simDataDir,filesep,inputFileName,ext],newFile);
-m = matfile(newFile,'Writable',true);
-m.timingResiduals = m.timingResiduals - estTimRes;
-toc
+% %% Set up
+% simDataDir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands';
+% estDataDir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/Results_supNar';
+% inputFileName = 'GWBsimDataSKASrlz1Nrlz3';
+% ext = '.mat';
+% 
+% 
+% % Load the simulated source parameters.
+% load([simDataDir,filesep,inputFileName,ext],'simParams','yr');
+% outputfiles = dir([estDataDir,filesep,num2str(stage - 1),'_',inputFileName,'*',ext]);
+% NestSrc = length(outputfiles);
+% outputfilenames = sort_nat({outputfiles.name});
+% 
+% estTimRes = zeros(simParams.Np,simParams.N);
+% ResCell = {}; % a cell of timing residuals to store all the estimated residuals.
+% % SNRarray = [];
+% 
+% for j = 1:NestSrc
+%     
+%     
+%     path_estData = [estDataDir,filesep,char(outputfilenames(j))];
+%     [srcParams]=ColSrcParams(path_estData);
+%     if srcParams.omega < searchRange.searchParams.angular_velocity(1,1) && srcParams.omega > range_low
+%         [~,estTimRes_tmp] = Amp2Snr(srcParams,simParams,yr);
+%         ResCell = [ResCell estTimRes_tmp];
+%         %     SNRarray = [SNRarray SNR];
+%     else
+%         continue
+%     end
+%     
+% end
+% 
+% % Accumulate the timing residulas for different sources.
+% num_src = length(ResCell);
+% for nsrc = 1:num_src
+%     estTimRes = estTimRes + ResCell{nsrc};
+% end
+% 
+% newFile = [outputDir,filesep,num2str(stage),'_',inputFileName,ext];
+% copyfile([simDataDir,filesep,inputFileName,ext],newFile);
+% m = matfile(newFile,'Writable',true);
+% m.timingResiduals = m.timingResiduals - estTimRes;
+% toc
 
 
 % END
---
+++ GENSIMDATA/searchParamsFluctuate.m
@@ -0,0 +1,43 @@
+% Read a searchParams.json file and fluctuate around the band boundary,
+% then generate new search parameter set.
+
+% QYQ 08/2020
+
+%%
+clear
+
+input_file_name = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/searchParams/2bands/searchParams1.json'; % file defines the band edge to be shifted
+s1 = jsondecode(fileread(input_file_name));
+NumBands = 2; % number of bands
+% create searchParams struct
+searchParams.alpha = s1.alpha;
+searchParams.delta = s1.delta;
+searchParams.angular_velocity = s1.angular_velocity;
+searchParams.phi0 = s1.phi0;
+searchParams.inclination = s1.inclination;
+searchParams.amplitude = s1.amplitude;
+searchParams.polarization = s1.polarization;
+searchParams.band_num = s1.band_num;
+
+
+full_range_file_name = 'Nyquist.json'; % file defines the whole search range
+fullRange = jsondecode(fileread(full_range_file_name));
+FreqRange = fullRange.angular_velocity;
+
+edge = s1.angular_velocity(1,1);
+fluctuation = 10 * randn; % fluctuation range
+new_edge = edge + fluctuation;
+
+s1.angular_velocity(1,1) = new_edge;
+[filePath,newFile,~] = fileparts(input_file_name);
+newFile = regexp(newFile,'[a-zA-Z]*','match');
+save([filePath,filesep,char(newFile),'Rand1','.mat'],'searchParams','NumBands','FreqRange');
+
+fullRange.angular_velocity(2,1) = new_edge;
+fullRange.band_num = 2;
+
+% update searchParams for 2nd band
+searchParams.angular_velocity = fullRange.angular_velocity;
+searchParams.band_num = fullRange.band_num;
+
+save([filePath,filesep,char(newFile),'Rand2','.mat'],'searchParams','NumBands','FreqRange')
