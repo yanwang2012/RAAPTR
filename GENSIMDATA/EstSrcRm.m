c6ecb6ab81bafa330315adef28d4bc57d5a1a4f4
Yiqian Qian <qyq@Yiqians-MBP.lan1>
2020-11-25 18:24:52 +0800

    remove "rassign" func and beautified code indent

--- GENSIMDATA/EstSrcRm.m
+++ GENSIMDATA/EstSrcRm.m
@@ -17,54 +17,53 @@ bandNum = 2;
 for i = 1:bandNum
     outputfiles = dir([estDataDir,filesep,num2str(i),'_',inputFileName,'*',ext]);
     NestSrc = length(outputfiles);
-if stage == 1
-    simDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2';
-    load([simDataDir,filesep,num2str(i),'_',inputFileName,ext])
-else
-    load([simDataDir,filesep,num2str(i),'_',inputFileName,'_after_',num2str(stage-1),'iMBLT',ext]);% subtraction in iMBLT needs to base on the new inputdata generated from last subtraction, not from the original data.
-end
-estTimRes = zeros(simParams.Np,simParams.N);
-ResCell = {}; % a cell of timing residuals to store all the estimated residuals.
-SNRarray = [];
-
-%% MBLT
-[file,Index]=rassign(estDataDir,outputfiles,NestSrc,simParams,yr);
-% disp(["File needs to be skipped: ",file]);
-foldername = [num2str(stage),'iMBLT_after'];
-OutputDir = [estDataDir,filesep,foldername];
-outputfilenames = sort_nat({outputfiles.name});
-
-mkdir(OutputDir);
-
-for j = 1:NestSrc
-    
-    if j == Index
-        continue
+    if stage == 1
+        simDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2';
+        load([simDataDir,filesep,num2str(i),'_',inputFileName,ext])
     else
+        load([simDataDir,filesep,num2str(i),'_',inputFileName,'_after_',num2str(stage-1),'iMBLT',ext]);% subtraction in iMBLT needs to base on the new inputdata generated from last subtraction, not from the original data.
+    end
+    estTimRes = zeros(simParams.Np,simParams.N);
+    ResCell = {}; % a cell of timing residuals to store all the estimated residuals.
+    SNRarray = [];
+    
+    %% MBLT
+    % [file,Index]=rassign(estDataDir,outputfiles,NestSrc,simParams,yr);
+    % disp(["File needs to be skipped: ",file]);
+    foldername = [num2str(stage),'iMBLT_after'];
+    OutputDir = [estDataDir,filesep,foldername];
+    outputfilenames = sort_nat({outputfiles.name});
+    
+    mkdir(OutputDir);
+    
+    for j = 1:NestSrc
+        
+        %         if j == Index
+        %             continue
+        %         else
         %                 disp("j is:"+j);
         path_estData = [estDataDir,filesep,char(outputfilenames(j))];
         [srcParams]=ColSrcParams(path_estData);
         [SNR,estTimRes_tmp] = Amp2Snr(srcParams,simParams,yr);
         ResCell = [ResCell estTimRes_tmp];
         SNRarray = [SNRarray SNR];
+        %         end
     end
     
-end
-
-% Accumulate the timing residulas for different sources.
-for nsrc = 1:NestSrc
-    estTimRes = estTimRes + ResCell{nsrc};
-end
-
-newFile = strcat(OutputDir,filesep,num2str(i),'_',inputFileName,'_after_',num2str(stage),'iMBLT',ext);
-
-if stage == 1
-    copyfile([simDataDir,filesep,num2str(i),'_',inputFileName,ext],newFile);
-else
-    copyfile([simDataDir,filesep,num2str(i),'_',inputFileName,'_after_',num2str(stage-1),'iMBLT',ext],newFile);
-end
-m = matfile(newFile,'Writable',true);
-m.timingResiduals = timingResiduals - estTimRes;
+    % Accumulate the timing residulas for different sources.
+    for nsrc = 1:NestSrc
+        estTimRes = estTimRes + ResCell{nsrc};
+    end
+    
+    newFile = strcat(OutputDir,filesep,num2str(i),'_',inputFileName,'_after_',num2str(stage),'iMBLT',ext);
+    
+    if stage == 1
+        copyfile([simDataDir,filesep,num2str(i),'_',inputFileName,ext],newFile);
+    else
+        copyfile([simDataDir,filesep,num2str(i),'_',inputFileName,'_after_',num2str(stage-1),'iMBLT',ext],newFile);
+    end
+    m = matfile(newFile,'Writable',true);
+    m.timingResiduals = timingResiduals - estTimRes;
 end
 
 
