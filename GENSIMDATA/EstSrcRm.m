ca7534693623dac9112d3edfa4979f8a8dddf164
Yiqian Qian <qyqstc@gmail.com>
2021-01-05 00:00:50 -0600

    LS5 commit

--- GENSIMDATA/EstSrcRm.m
+++ GENSIMDATA/EstSrcRm.m
@@ -6,11 +6,11 @@
 clear;
 tic
 %% Set up
-simDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2';
-estDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2/results/1_iMBLT/results';
+simDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2/results/1_iMBLT/results/1iMBLT_after/results/2_iMBLT/results/2iMBLT_after/results/3_iMBLT/results/3iMBLT_after/results/4_iMBLT/results/4iMBLT_after/results/5_iMBLT/results/5iMBLT_after/results/6_iMBLT/results/6iMBLT_after/results/7_iMBLT/results/7iMBLT_after/results/8_iMBLT/results/8iMBLT_after/results/9_iMBLT/results/9iMBLT_after/results/10_iMBLT/results/10iMBLT_after/results/11_iMBLT/results/11iMBLT_after/results/12_iMBLT/results/12iMBLT_after/results/13_iMBLT/results/13iMBLT_after/results/14_iMBLT/results/14iMBLT_after/results/15_iMBLT/results/15iMBLT_after/results/16_iMBLT/results/16iMBLT_after/results/17_iMBLT/results/17iMBLT_after/results/18_iMBLT/results/18iMBLT_after';
+estDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2/results/1_iMBLT/results/1iMBLT_after/results/2_iMBLT/results/2iMBLT_after/results/3_iMBLT/results/3iMBLT_after/results/4_iMBLT/results/4iMBLT_after/results/5_iMBLT/results/5iMBLT_after/results/6_iMBLT/results/6iMBLT_after/results/7_iMBLT/results/7iMBLT_after/results/8_iMBLT/results/8iMBLT_after/results/9_iMBLT/results/9iMBLT_after/results/10_iMBLT/results/10iMBLT_after/results/11_iMBLT/results/11iMBLT_after/results/12_iMBLT/results/12iMBLT_after/results/13_iMBLT/results/13iMBLT_after/results/14_iMBLT/results/14iMBLT_after/results/15_iMBLT/results/15iMBLT_after/results/16_iMBLT/results/16iMBLT_after/results/17_iMBLT/results/17iMBLT_after/results/18_iMBLT/results/18iMBLT_after/results/19_iMBLT/results';
 inputFileName = 'GWBsimDataSKASrlz1Nrlz3';
 ext = '.mat';
-stage = 1; % stage number of iMBLT
+stage = 19; % stage number of iMBLT
 bandNum = 2;
 
 % Load the simulated source parameters.
--- GENSIMDATA/iMBLT.m
+++ GENSIMDATA/iMBLT.m
@@ -4,11 +4,11 @@ clear;
 tic
 %% Set up
 simDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/simData/Union2_xMBLT_iMBLT';
-iMBLTDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union_xMBLT2/results/1_iMBLT/results/1iMBLT_after/results/2_iMBLT/results/2iMBLT_after/results/3_iMBLT/results/3iMBLT_after/results/4_iMBLT/results/4iMBLT_after/results/5_iMBLT/results/5iMBLT_after/results/6_iMBLT/results/6iMBLT_after/results/7_iMBLT/results/7iMBLT_after/results/8_iMBLT/results/8iMBLT_after/results/9_iMBLT/results/9iMBLT_after/results/10_iMBLT/results/10iMBLT_after/results/11_iMBLT/results/11iMBLT_after/results/12_iMBLT/results/12iMBLT_after/results/13_iMBLT/results/13iMBLT_after/results/14_iMBLT/results/14iMBLT_after/results/15_iMBLT/results/15iMBLT_after/results/16_iMBLT/results/16iMBLT_after/results/17_iMBLT/results/17iMBLT_after/results/18_iMBLT/results/18iMBLT_after';
-estDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2/results';
+iMBLTDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2/results/1_iMBLT/results/1iMBLT_after/results/2_iMBLT/results/2iMBLT_after/results/3_iMBLT/results/3iMBLT_after/results/4_iMBLT/results/4iMBLT_after/results/5_iMBLT/results/5iMBLT_after/results/6_iMBLT/results/6iMBLT_after/results/7_iMBLT/results/7iMBLT_after/results/8_iMBLT/results/8iMBLT_after/results/9_iMBLT/results/9iMBLT_after/results/10_iMBLT/results/10iMBLT_after/results/11_iMBLT/results/11iMBLT_after/results/12_iMBLT/results/12iMBLT_after/results/13_iMBLT/results/13iMBLT_after/results/14_iMBLT/results/14iMBLT_after/results/15_iMBLT/results/15iMBLT_after/results/16_iMBLT/results/16iMBLT_after/results/17_iMBLT/results/17iMBLT_after/results/18_iMBLT/results/18iMBLT_after';
+estDataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/BANDEDGE/2bands/superNarrow/Union2_xMBLT/results/Union2_xMBLT2/results/1_iMBLT/results/1iMBLT_after/results/2_iMBLT/results/2iMBLT_after/results/3_iMBLT/results/3iMBLT_after/results/4_iMBLT/results/4iMBLT_after/results/5_iMBLT/results/5iMBLT_after/results/6_iMBLT/results/6iMBLT_after/results/7_iMBLT/results/7iMBLT_after/results/8_iMBLT/results/8iMBLT_after/results/9_iMBLT/results/9iMBLT_after/results/10_iMBLT/results/10iMBLT_after/results/11_iMBLT/results/11iMBLT_after/results/12_iMBLT/results/12iMBLT_after/results/13_iMBLT/results/13iMBLT_after/results/14_iMBLT/results/14iMBLT_after/results/15_iMBLT/results/15iMBLT_after/results/16_iMBLT/results/16iMBLT_after/results/17_iMBLT/results/17iMBLT_after/results/18_iMBLT/results/18iMBLT_after/results';
 inputFileName = 'GWBsimDataSKASrlz1Nrlz3';
 bandNum = 2; % number of band
-stage = 1; % iMBLT stages
+stage = 19; % iMBLT stages
 for band = 1:bandNum
     if bandNum > 1
         outputfiles = dir([estDataDir,filesep,num2str(band),'_',inputFileName,'*.mat']);
@@ -19,8 +19,8 @@ for band = 1:bandNum
     NestSrc = length(outputfiles);
     
     % Load the simulated source parameters.
-    load([simDataDir,filesep,num2str(band),'_',inputFileName,'.mat']); % for first iMBLT stage.
-    % load([iMBLTDataDir,filesep,num2str(band),'_',inputFileName,'_after_',num2str(stage-1),'iMBLT','.mat'])
+    % load([simDataDir,filesep,num2str(band),'_',inputFileName,'.mat']); % for first iMBLT stage.
+    load([iMBLTDataDir,filesep,num2str(band),'_',inputFileName,'_after_',num2str(stage-1),'iMBLT','.mat'])
     estTimRes = zeros(simParams.Np,simParams.N);
     ResCell = {}; % a cell of timing residuals to store all the estimated residuals.
     SNRarray = [];
@@ -66,8 +66,8 @@ for band = 1:bandNum
     
     % remove sources from input data
     newFile = strcat(OutputDir,filesep,num2str(band),'_',inputFileName,'_',num2str(stage),'iMBLT','.mat');
-    copyfile([simDataDir,filesep,num2str(band),'_',inputFileName,'.mat'],newFile); % for first iMBLT stage
-    % copyfile([iMBLTDataDir,filesep,num2str(band),'_',inputFileName,'_after_',num2str(stage-1),'iMBLT','.mat'],newFile); % can't use * to match files
+    % copyfile([simDataDir,filesep,num2str(band),'_',inputFileName,'.mat'],newFile); % for first iMBLT stage
+    copyfile([iMBLTDataDir,filesep,num2str(band),'_',inputFileName,'_after_',num2str(stage-1),'iMBLT','.mat'],newFile); % can't use * to match files
     
     m = matfile(newFile,'Writable',true); % .mat file needs to be V7.3, if can't load, use command save('filename','-v7.3') convert
     m.timingResiduals = timingResiduals - estTimRes;
