6d1ba1f67fb469df1e8e03babb66acf96e199b0e
Yiqian Qian <qyq@Yiqians-MBP.lan1>
2021-01-06 16:21:05 +0800

    made code more readable

--- GENSIMDATA/genls5launcherjobfile.m
+++ GENSIMDATA/genls5launcherjobfile.m
@@ -1,7 +1,7 @@
 clear;
-simDataDir = '/Users/qianyiqian/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Final/realizations/2bands/HDF5'; % local
+simDataDir = '/Users/qyq/Research/PulsarTiming/YuYang_data/HDF5'; % local
 %simDataDir = '/work/05884/qyqstc/lonestar/GWBsimDataSKA_HDF5'; % on ls5
-search_paramDataDir = '~/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/searchParams/2bands/HDF5';
+search_paramDataDir = '/Users/qyq/Research/PulsarTiming/YuYang_data/searchParams';
 inFileList = dir([simDataDir,filesep,'GWB*','.hdf5']);% original file
 cpyFileList = dir([simDataDir,filesep,'*_GWB*','.hdf5']);% total files including duplicate files for different bands.
 inFileList_param = dir([search_paramDataDir,filesep,'*','.hdf5']);
@@ -15,14 +15,14 @@ else
     nFiles = length(inFileList); % Single band
     inFileName = sort_nat({inFileList.name}); 
 end
-fid = fopen('~/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Final/2bandRealizations.txt','w');% 'a' for appending data at the end of the file.
+fid = fopen('/Users/qyq/Research/PulsarTiming/YuYang_data/JOBFILE.txt','w');% 'a' for appending data at the end of the file.
 
 %% LS5 config
 excut = '/work/05884/qyqstc/lonestar/RAAPTR/MxAvPhaseC/Multi_PSO.out ';
-searchParamDir = '/work/05884/qyqstc/lonestar/MultiPSO/Task8/searchParams/2bands/HDF5/';
-dataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Final/realizations/2bands/';
+searchParamDir = '/work/05884/qyqstc/lonestar/MultiPSO/Yuyang/searchParams/';
+dataDir = '/work/05884/qyqstc/lonestar/MultiPSO/Yuyang/';
 resultsDir = [dataDir,'results/'];
-ite = 20;
+ite = 3;
 
 %% single source
 % for lpc = 1:nFiles
@@ -45,16 +45,16 @@ ite = 20;
 %     inFileName = [inFileName cpyFileList(k).name];
 % end
 
-bandFiles = nFiles/nParamFiles;
+realizations = nFiles/nParamFiles; 
 
 for lpc = 1:nParamFiles
-    for ppc = 1:bandFiles
+    for ppc = 1:realizations
         bandNum = h5readatt([search_paramDataDir,'/',inFileList_param(lpc).name],...
             ['/','Bands_Info'],'bandNumber');
         fprintf(fid,excut);
         fprintf(fid,searchParamDir + "%s ",inFileList_param(lpc).name); % double quotes create string, "plus" combine strings.
-        fprintf(fid,dataDir + "%s ",char(inFileName((lpc-1) * bandFiles + ppc)));
-        fprintf(fid,resultsDir + "%s ",char(inFileName((lpc-1) * bandFiles + ppc)));
+        fprintf(fid,dataDir + "%s ",char(inFileName((lpc-1) * realizations + ppc)));
+        fprintf(fid,resultsDir + "%s ",char(inFileName((lpc-1) * realizations + ppc)));
         fprintf(fid,'avPhase ');
         fprintf(fid,'%d',ite); % number of iteration
         fprintf(fid,'\n'); % if this line is the last line delete the blank line, it will affect Launcher counting the number of jobs.
