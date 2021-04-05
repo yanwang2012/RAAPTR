90c2367324d486159492964e5f4f197d9c41c6a2
Yiqian Qian <qyq@Yiqians-MBP.lan1>
2021-04-05 16:30:47 +0800

    combine scatter plot with surf

--- GENSIMDATA/plot_id_true.m
+++ GENSIMDATA/plot_id_true.m
@@ -7,7 +7,7 @@
 clear;
 %% Load data
 simDataDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/Band_opt/simData';
-idDataDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/Band_opt/iMBLT/results';
+idDataDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/Band_opt/iMBLT';
 Filename = 'GWBsimDataSKASrlz1Nrlz3';
 identifyFilename = 'SelectedSrc';
 ext = '.mat';
@@ -84,34 +84,61 @@ saveas(gcf,[idDataDir,filesep,'fig',filesep,'SkyLocationC.png'])
 savefig([idDataDir,filesep,'fig',filesep,'SkyLocationC'])
 
 %% 3D sphere plot
-figure
-[x,y,z] = sphere; % generate a unit sphere
+load([idDataDir,filesep,'Acond.mat']); % load skymap condition number
+
+[x,y,z] = sphere(300); % generate a 300 faces unit sphere
 
 % convert sources coord. to spherical
 [id_x,id_y,id_z] = sph2cart(idRA,idDec,1);
 [sim_x,sim_y,sim_z] = sph2cart(simRA_nm,simDec_nm,1);
 [matched_x,matched_y,matched_z] = sph2cart(matched_alpha,matched_dec,1);
 
-surf(x,y,z,'LineStyle','--','FaceColor','white')
+figure
+
+ax1 = axes;
+surf(ax1,x,y,z,Acond,'EdgeColor','Interp','FaceColor','Interp');
 axis equal
-% pause
+
+ax2 = axes;
+scatter3(ax2,id_x,id_y,id_z,40,idSNR,'filled');
 hold on
-scatter3(id_x,id_y,id_z,40,idSNR,'filled')
-scatter3(sim_x,sim_y,sim_z,'o','MarkerEdgeColor','#888E8F')
-scatter3(matched_x,matched_y,matched_z,[],matched_snr,'s')
-c = colorbar;
-ylabel(c,'SNR','FontSize',14)
+scatter3(ax2,sim_x,sim_y,sim_z,'o','MarkerEdgeColor','#888E8F')
+scatter3(ax2,matched_x,matched_y,matched_z,[],matched_snr,'s');
 
 % connect identified sources with matched true sources with great circle on
 % sphere
 center = zeros(1,3); % center of sphere
 for src = 1:length(idRA)
 [v] = GreatCircle(center,[id_x(src),id_y(src),id_z(src)],[matched_x(src),matched_y(src),matched_z(src)],1);
-plot3(v(1,:),v(2,:),v(3,:),'m')
-pause
+plot3(ax2,v(1,:),v(2,:),v(3,:),'m')
+% pause
 end
 
+axis equal
+
+% Link two axes together
+hLink = linkprop([ax1,ax2],{'XLim','YLim','ZLim','CameraUpVector','CameraPosition','CameraTarget'});
+
+% Hide the top axes
+ax2.Visible = 'off';
+ax2.XTick = [];
+ax2.YTick = [];
+
+% Give each one its colormap
+colormap(ax1)
+colormap(ax2,'spring')
+
+% get everthin lined up
+cb1 = colorbar(ax1,'Position',[0.1 0.1 0.05 0.815]); % four-elements vector to specify Position [left bottom width height]
+cb2 = colorbar(ax2,'Position',[0.81 0.1 0.05 0.815]);
+cb1.Label.String = 'Condition Number';
+cb2.Label.String = 'SNR';
+cb1.Label.FontSize = 14;
+cb2.Label.FontSize = 14;
+
 legend({'Grid','Identified Sources','True Sources','Matched True Source','Matched & Identi.'},'Location','southeast')
+setappdata(gcf,'StoreTheLink',hLink); % store the link so that they can rotate and zoom synchronically
+
 saveas(gcf,[idDataDir,filesep,'fig',filesep,'SkyLocation3D.png'])
 savefig([idDataDir,filesep,'fig',filesep,'SkyLocation3D'])
 
