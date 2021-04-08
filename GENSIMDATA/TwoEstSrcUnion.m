1cf5cb89c8d5721f48b7e16808f329ce1c8a882c
Yiqian Qian <qyq@Yiqians-MBP.lan1>
2021-04-08 20:23:29 +0800

    extend to handle multiple relizations

--- GENSIMDATA/TwoEstSrcUnion.m
+++ GENSIMDATA/TwoEstSrcUnion.m
@@ -10,100 +10,116 @@ clear;
 tic
 
 %% Dir settings
-simdataDir = '~/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands';
-estSrc1Dir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/SuperNarrow/Results_supNar/GWBsimDataSKASrlz1Nrlz3_xMBLT/results';
+simdataDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Final/realizations/2bands/simData';
+estSrc1Dir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Final/realizations/2bands/xMBLT_set1/results';
 estsrc1 = 'supNarxMBLT';
-estSrc2Dir = '/Users/qyq/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/SuperNarrow/Results_supNar_rand1/GWBsimDataSKASrlz1Nrlz3_xMBLT/results';
+estSrc2Dir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Final/realizations/2bands/xMBLT_set2/results';
 estsrc2 = 'supNarxMBLTRand1';
-Filename = 'GWBsimDataSKASrlz1Nrlz3';
+Filename = 'GWBsimDataSKASrlz1Nrlz';
 ext = '.mat';
 
 %% Files
 estSrc1File = dir([estSrc1Dir,filesep,'*',Filename,'*',ext]);
 estSrc2File = dir([estSrc2Dir,filesep,'*',Filename,'*',ext]);
-Nestsrc = length(estSrc2File);
+simFile = dir([simdataDir,filesep,Filename,'*',ext]);
 estSrc2Filename = sort_nat({estSrc2File.name});
 estSrc1Filename = sort_nat({estSrc1File.name});
-simFile = [simdataDir,filesep,Filename,ext];
-load(simFile);
-%% Get estimated sources info
-EstSrc2 = {};
-EstSrc1 = {};
+simFilename = sort_nat({simFile.name});
 
-for k = 1:Nestsrc
-    path_to_estimatedDataestSrc2 = [estSrc2Dir,filesep,char(estSrc2Filename(k))];
-    path_to_estimatedDataestSrc1 = [estSrc1Dir,filesep,char(estSrc1Filename(k))];
-    EstSrc2{k} = ColSrcParams(path_to_estimatedDataestSrc2);
-    EstSrc1{k} = ColSrcParams(path_to_estimatedDataestSrc1);
+Nestsrc = length(estSrc1File);
+Nrlzs = length(simFile);
+Nite = Nestsrc/Nrlzs;
+
+for rlz = 1:4%Nrlzs
+    load([simdataDir,filesep,simFilename{rlz}]);
+    %% Get estimated sources info
+    EstSrc2 = {};
+    EstSrc1 = {};
+    exp = ['\d+_',Filename,num2str(rlz),'(?=_|\.mat)_?\d{0,2}.mat']; % regular expression for multiple realizations
+    estSrc1File_tmp = regexp(estSrc1Filename,exp,'match');
+    estSrc1File_tmp = estSrc1File_tmp(~cellfun(@isempty, estSrc1File_tmp));
+    estSrc2File_tmp = regexp(estSrc2Filename,exp,'match');
+    estSrc2File_tmp = estSrc2File_tmp(~cellfun(@isempty, estSrc2File_tmp));
+    for k = 1:Nite
+        path_to_estimatedDataestSrc2 = [estSrc2Dir,filesep,estSrc2File_tmp{k}{1}];
+        path_to_estimatedDataestSrc1 = [estSrc1Dir,filesep,estSrc1File_tmp{k}{1}];
+        EstSrc2{k} = ColSrcParams(path_to_estimatedDataestSrc2, simParams.Np);
+        EstSrc1{k} = ColSrcParams(path_to_estimatedDataestSrc1, simParams.Np);
+    end
+    
+    % Cross-Corelation
+    [gamma,rho,id_max,estSNR1,estSNR2] = ESNMTCW(Nite,EstSrc1,EstSrc2,simParams,yr,0.90);
+    
+    
+    %% find highly correlated sources
+    t = 0.80; % NMTC threshold used to select sources.
+    [r,c,~] = find(gamma > t); % r is the row of rho, c is the column of rho.
+    % in gamma, rows correspond to EstSrc2, columns correspond to EstSrc1.
+    % map = [r c]; % create a map between EstSrc1 & EstSrc2.
+    
+    %% Take union of two Est. sets
+    EstSrc1d = EstSrc1(setdiff(1:Nite,c)); % sources in EstSrc1 which indices are not in c
+    EstSrc2d = EstSrc2(setdiff(1:Nite,r)); % ...........EstSrc2..........................r
+    estSNR1d = estSNR1(setdiff(1:Nite,c));
+    estSNR2d = estSNR2(setdiff(1:Nite,r));
+    
+    UnSrc = [EstSrc1d EstSrc2]; % Union of 2 sets of est. sources with the choice of EstSrc2(r) as the combined highly correlated sources.
+    UnSNR = cat(1,estSNR1d,estSNR2);
+    
+    %% copy files together
+    folderName = 'Union';
+    [~,simFile_noExt,~] = fileparts(simFilename{rlz});
+    NewFolder = [estSrc2Dir,filesep,folderName,filesep,simFile_noExt];
+    mkdir(NewFolder);
+    for files = 1:Nite
+        copyfile([estSrc2Dir,filesep,estSrc2File_tmp{files}{1}],NewFolder); % copy all files from est. sources 2
+    end
+    
+    % Copy EstSrc1 which are not highly correlated with EstSrc2.
+    id_diff = setdiff(1:Nite,c); % get indices of est. srouces 1 not in map.
+    for i = 1:length(id_diff)
+        newFilename = [NewFolder,filesep,'diff_',estSrc1File_tmp{id_diff(i)}{1}];
+        copyfile([estSrc1Dir,filesep,estSrc1File_tmp{id_diff(i)}{1}],newFilename);
+    end
+    
+    
+    %% Plotting
+    % Uncomment to check for a specific realization.
+    
+    % metric = 'NMTC';
+    % methods = 'supNarxMBLT-supNarxMBLTRand1-ALL';
+    % prefix = [estSrc2Dir,filesep,folderName,filesep,'fig',filesep,metric,'-',methods];
+    % mkdir(prefix);
+    %
+    % figname = 'NMTC';
+    %
+    % figure
+    % imagesc(gamma);
+    % colorbar
+    % xlabel(estsrc1)
+    % ylabel(estsrc2)
+    % title(figname)
+    % saveas(gcf,[prefix,filesep,figname],'png');
+    % savefig([prefix,filesep,figname]);
+    %
+    % figname = 'Uinon vs True';
+    %
+    % figure
+    % plot(snr_chr,omega/(365*24*3600*2*pi),'ob')
+    % hold on
+    % for i = 1:length(UnSrc)
+    %     plot(UnSNR(i),UnSrc{i}.omega/(365*24*3600*2*pi),'sr')
+    % end
+    % hold off
+    %
+    % title('Union vs. True')
+    % xlabel('SNR')
+    % ylabel('Frequency')
+    % legend('True','Union')
+    % saveas(gcf,[prefix,filesep,figname],'png')
+    % savefig([prefix,filesep,figname])
 end
-
-% Cross-Corelation
-[gamma,rho,id_max,estSNR1,estSNR2] = ESNMTCW(Nestsrc,EstSrc1,EstSrc2,simParams,yr,0.90);
-
-
-%% find highly correlated sources
-t = 0.80; % NMTC threshold used to select sources.
-[r,c,~] = find(gamma > t); % r is the row of rho, c is the column of rho.
-% in gamma, rows correspond to EstSrc2, columns correspond to EstSrc1.
-% map = [r c]; % create a map between EstSrc1 & EstSrc2.
-
-%% Take union of two Est. sets
-EstSrc1d = EstSrc1(setdiff(1:Nestsrc,c)); % sources in EstSrc1 which indices are not in c
-EstSrc2d = EstSrc2(setdiff(1:Nestsrc,r)); % ...........EstSrc2..........................r
-estSNR1d = estSNR1(setdiff(1:Nestsrc,c));
-estSNR2d = estSNR2(setdiff(1:Nestsrc,r));
-
-UnSrc = [EstSrc1d EstSrc2]; % Union of 2 sets of est. sources with the choice of EstSrc2(r) as the combined highly correlated sources.
-UnSNR = cat(1,estSNR1d,estSNR2);
-
-%% copy files together
-folderName = 'Union2';
-NewFolder = [estSrc2Dir,filesep,folderName];
-copyfile([estSrc2Dir,filesep,'*.mat'],NewFolder); % copy all files from est. sources 2
-
-% Copy EstSrc1 which are not highly correlated with EstSrc2.
-id_diff = setdiff(1:Nestsrc,c); % get indices of est. srouces 1 not in map. 
-des = [NewFolder,filesep,'EstSrc1'];
-mkdir(des)
-
-for i = 1:length(id_diff)
-    copyfile([estSrc1Dir,filesep,estSrc1Filename{id_diff(i)}],des);
-end
-
-%% Plotting
-metric = 'NMTC';
-methods = 'supNarxMBLT-supNarxMBLTRand1-ALL';
-prefix = [estSrc2Dir,filesep,folderName,filesep,'fig',filesep,metric,'-',methods];
-mkdir(prefix);
-
-figname = 'NMTC';
-
-figure
-imagesc(gamma);
-colorbar
-xlabel(estsrc1)
-ylabel(estsrc2)
-title(figname)
-saveas(gcf,[prefix,filesep,figname],'png');
-savefig([prefix,filesep,figname]);
-
-figname = 'Uinon vs True';
-
-figure
-plot(snr_chr,omega/(365*24*3600*2*pi),'ob')
-hold on
-for i = 1:length(UnSrc)
-   plot(UnSNR(i),UnSrc{i}.omega/(365*24*3600*2*pi),'sr') 
-end
-hold off
-
-title('Union vs. True')
-xlabel('SNR')
-ylabel('Frequency')
-legend('True','Union')
-saveas(gcf,[prefix,filesep,figname],'png')
-savefig([prefix,filesep,figname])
-
 
 toc
+
 %END
--- GENSIMDATA/freqGap.m
+++ GENSIMDATA/freqGap.m
@@ -6,66 +6,74 @@
 %% Data Dir
 clear
 
-estSrcDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/BANDEDGE/2bands/SuperNarrow/Results_supNar_rand1/GWBsimDataSKASrlz1Nrlz3_xMBLT/results/Union2';
-Filename = 'GWBsimDataSKASrlz1Nrlz3';
-ext = '.mat';
+UnionDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Final/realizations/2bands/xMBLT_set2/results/Union';
+simDataDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Final/realizations/2bands/simData';
+outDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Final/realizations/2bands/xMBLT_set2/results/Band_opt';
 
-estSrcFile = dir([estSrcDir,filesep,'*',Filename,'*',ext]);
-estSrcFilename = sort_nat({estSrcFile.name});
-Nfiles = length(estSrcFile);
+simFiles = dir([simDataDir,filesep,'*.mat']);
+simFilenames = sort_nat({simFiles.name});
+Nrlzs = length(simFiles);
 
-%% load estimated sources
-Np = 1000; % # of pulsars used in simulation
-estSrc = {};
-estFreq = zeros(1,Nfiles);
-
-for i = 1:Nfiles
-    path_to_estSrc = [estSrcDir,filesep,estSrcFilename{i}];
-    estSrc{i} = ColSrcParams(path_to_estSrc,Np);
-    estFreq(i) = estSrc{i}.omega/(2*pi*365*24*3600); % convert rad/year -> Hz
-end
-
-estFreq = sort(estFreq); % sort frequencies in ascending order
-
-% calculate joint freq difference
-difreq = zeros(1,Nfiles);
-for j = 1:Nfiles - 1
-    difreq(j) = abs(estFreq(j+1) - estFreq(j))/estFreq(j); % relative frequency gap
-end
-
-threshold = 0.4;
-idx = find(difreq > 0.4 & difreq < 0.9);
-bandedge = (1 + 1/2 * difreq(idx)) .* estFreq(idx);
-bandedge_av = bandedge * 365*24*3600*2*pi; % convert Hz to rad/year
-
-%% Generate search file
-filename = 'Nyquist.json';
-outDir = '/Users/qyq/Library/Mobile Documents/com~apple~CloudDocs/Research/PulsarTiming/SimDATA/MultiSource/Investigation/Test11/searchParams/Band_opt/New';
-mkdir(outDir);
-searchParams = jsondecode(fileread(filename));
-NumBands = length(idx);%5; %10; % total number of bands
-% bandwidth = bandedge_av;
-FreqRange = searchParams.angular_velocity;
-for i = 1:NumBands + 1 % i band edges can split whole band into i+1 segments
-    if i == NumBands + 1
-        searchParams.angular_velocity(1) = FreqRange(1);
-        save([outDir,filesep,'searchParams_Nyquist',num2str(i),'.mat'],'searchParams','NumBands','FreqRange');
-    else
-        searchParams.angular_velocity(1) = bandedge_av(i); % update upper limit
-        searchParams.band_num = i;
-        save([outDir,filesep,'searchParams_Nyquist',num2str(i),'.mat'],'searchParams','NumBands','FreqRange');
-        tmp = searchParams.angular_velocity(1); % save upper limit for prev. band
-        searchParams.angular_velocity(2) = tmp; % update lower limit
+for rlz = 1:4 %Nrlzs
+    [~,simFile_noExt,~] = fileparts(simFilenames{rlz});
+    estSrcFile = dir([UnionDir,filesep,simFile_noExt,filesep,'*.mat']);
+    estSrcFilename = sort_nat({estSrcFile.name});
+    Nfiles = length(estSrcFile);
+    
+    %% load estimated sources
+    Np = 1000; % # of pulsars used in simulation
+    estSrc = {};
+    estFreq = zeros(1,Nfiles);
+    
+    for i = 1:Nfiles
+        path_to_estSrc = [UnionDir,filesep,simFile_noExt,filesep,estSrcFilename{i}];
+        estSrc{i} = ColSrcParams(path_to_estSrc,Np);
+        estFreq(i) = estSrc{i}.omega/(2*pi*365*24*3600); % convert rad/year -> Hz
     end
+    
+    estFreq = sort(estFreq); % sort frequencies in ascending order
+    
+    % calculate joint freq difference
+    difreq = zeros(1,Nfiles);
+    for j = 1:Nfiles - 1
+        difreq(j) = abs(estFreq(j+1) - estFreq(j))/estFreq(j); % relative frequency gap
+    end
+    
+    threshold = 0.4;
+    idx = find(difreq > 0.25 & difreq < 0.9);
+    bandedge = (1 + 1/2 * difreq(idx)) .* estFreq(idx);
+    bandedge_av = bandedge * 365*24*3600*2*pi; % convert Hz to rad/year
+    
+    %% Generate search file
+    filename = 'Nyquist.json';
+    searchParams = jsondecode(fileread(filename));
+    NumBands = length(idx) + 1;%5; %10; % total number of bands
+    % bandwidth = bandedge_av;
+    FreqRange = searchParams.angular_velocity;
+    mkdir([outDir,filesep,simFile_noExt]);
+    
+    for i = 1:NumBands % i band edges can split whole band into i+1 segments
+        if i == NumBands
+            searchParams.angular_velocity(1) = FreqRange(1);
+            save([outDir,filesep,simFile_noExt,filesep,'searchParams_Nyquist',num2str(i),'.mat'],'searchParams','NumBands','FreqRange');
+        else
+            searchParams.angular_velocity(1) = bandedge_av(i); % update upper limit
+            searchParams.band_num = i;
+            save([outDir,filesep,simFile_noExt,filesep,'searchParams_Nyquist',num2str(i),'.mat'],'searchParams','NumBands','FreqRange');
+            tmp = searchParams.angular_velocity(1); % save upper limit for prev. band
+            searchParams.angular_velocity(2) = tmp; % update lower limit
+        end
+    end
+    
+    
+    %% plot
+    figure
+    plot(estFreq,difreq,'ro')
+    xlabel('Frequency [Hz]')
+    ylabel('Relative Freq. Diff')
+    title('Frequency Gap')
+    % saveas(gcf,[estSrcDir,filesep,'fig',filesep,'FreqGap.png'])
+    % savefig([estSrcDir,filesep,'fig',filesep,'FreqGap'])
 end
 
-%% plot
-figure
-plot(estFreq,difreq,'ro')
-xlabel('Frequency [Hz]')
-ylabel('Relative Freq. Diff')
-title('Frequency Gap')
-saveas(gcf,[estSrcDir,filesep,'fig',filesep,'FreqGap.png'])
-savefig([estSrcDir,filesep,'fig',filesep,'FreqGap'])
-
-%EOF
+%END
