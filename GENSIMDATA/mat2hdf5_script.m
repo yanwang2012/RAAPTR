9380841f8dec0162a168c5fc6921c4c71836d706
Yiqian Qian <qyqstc@login2.stampede2.tacc.utexas.edu>
2021-06-02 00:25:12 -0500

    update from stampede2

--- GENSIMDATA/mat2hdf5_script.m
+++ GENSIMDATA/mat2hdf5_script.m
@@ -1,6 +1,6 @@
 % convert .mat file to .hdf5 file
 clear;
-DataDir = '/work2/05884/qyqstc/stampede2/MultiPSO/Final/realizations/2bands/results_diff_opt_xMBLT2/1_iMBLT';
+DataDir = '/work2/05884/qyqstc/stampede2/MultiPSO/Final/realizations/2bands/results_diff_opt_xMBLT2/1_iMBLT/results/1iMBLT_after';
 outDir = [DataDir,filesep,'HDF5'];
 inFileList = dir([DataDir,filesep,'*GWB*.mat']); % input file directory
 mkdir(outDir); % output directory
